ce2d857842af6e440e6f53a6937dca289c635286
B33 <my@email.com>
2020-02-06 07:35:39 -0600

    Add Comments

--- LoopsAndMessageBox/Form1.cs
+++ LoopsAndMessageBox/Form1.cs
@@ -21,6 +21,7 @@ namespace LoopsAndMessageBox
     {
       DialogResult selected = MessageBox.Show("Do you want to see some MessageBox dialogs", "While", MessageBoxButtons.YesNo);
 
+      // While loop. It does things while a statement is true. If the statement is never true, the code is never used.
       while(selected == DialogResult.Yes)
       {
         selected = MessageBox.Show("In our hubris we undermine our greatest efforts to prevail over what we perceived as unworthy of us.", "While", MessageBoxButtons.YesNo);
@@ -31,6 +32,7 @@ namespace LoopsAndMessageBox
     {
       DialogResult selected;
 
+      // Do loop. It does, and then checks to see if it should do again. It's code will always be run.
       do
       {
         selected = MessageBox.Show("No matter how great our advances, in the end we must reap what we sow.", "Do", MessageBoxButtons.YesNo);
@@ -39,9 +41,10 @@ namespace LoopsAndMessageBox
 
     private void btnFor_Click(object sender, EventArgs e)
     {
+      // For loop. Does something for X number of times.
       for (int i = 5; i > 0; i--)
       {
-        MessageBox.Show("The great hights we have risen to just means we have further to fall. " + i, "Do");
+        MessageBox.Show("The great hights we have risen to just means we have further to fall. " + i * 10, "Do");
       }
     }
   }
