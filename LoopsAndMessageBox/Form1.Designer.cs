ad101cac14d13963358cb5146b23d022585e5fa2
B33 <my@email.com>
2020-02-05 20:02:03 -0600

    Add project files.

---
+++ LoopsAndMessageBox/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace LoopsAndMessageBox
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.btnDo = new System.Windows.Forms.Button();
+      this.btnWhile = new System.Windows.Forms.Button();
+      this.btnFor = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // btnDo
+      // 
+      this.btnDo.Location = new System.Drawing.Point(226, 64);
+      this.btnDo.Name = "btnDo";
+      this.btnDo.Size = new System.Drawing.Size(126, 38);
+      this.btnDo.TabIndex = 0;
+      this.btnDo.Text = "Do";
+      this.btnDo.UseVisualStyleBackColor = true;
+      this.btnDo.Click += new System.EventHandler(this.btnDo_Click);
+      // 
+      // btnWhile
+      // 
+      this.btnWhile.Location = new System.Drawing.Point(57, 64);
+      this.btnWhile.Name = "btnWhile";
+      this.btnWhile.Size = new System.Drawing.Size(126, 38);
+      this.btnWhile.TabIndex = 1;
+      this.btnWhile.Text = "While";
+      this.btnWhile.UseVisualStyleBackColor = true;
+      this.btnWhile.Click += new System.EventHandler(this.btnWhile_Click);
+      // 
+      // btnFor
+      // 
+      this.btnFor.Location = new System.Drawing.Point(396, 64);
+      this.btnFor.Name = "btnFor";
+      this.btnFor.Size = new System.Drawing.Size(126, 38);
+      this.btnFor.TabIndex = 2;
+      this.btnFor.Text = "For";
+      this.btnFor.UseVisualStyleBackColor = true;
+      this.btnFor.Click += new System.EventHandler(this.btnFor_Click);
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(590, 159);
+      this.Controls.Add(this.btnFor);
+      this.Controls.Add(this.btnWhile);
+      this.Controls.Add(this.btnDo);
+      this.Name = "Form1";
+      this.Text = "Lööps";
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button btnDo;
+    private System.Windows.Forms.Button btnWhile;
+    private System.Windows.Forms.Button btnFor;
+  }
+}
+
---
+++ LoopsAndMessageBox/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoopsAndMessageBox
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void btnWhile_Click(object sender, EventArgs e)
+    {
+      DialogResult selected = MessageBox.Show("Do you want to see some MessageBox dialogs", "While", MessageBoxButtons.YesNo);
+
+      while(selected == DialogResult.Yes)
+      {
+        selected = MessageBox.Show("In our hubris we undermine our greatest efforts to prevail over what we perceived as unworthy of us.", "While", MessageBoxButtons.YesNo);
+      }
+    }
+
+    private void btnDo_Click(object sender, EventArgs e)
+    {
+      DialogResult selected;
+
+      do
+      {
+        selected = MessageBox.Show("No matter how great our advances, in the end we must reap what we sow.", "Do", MessageBoxButtons.YesNo);
+      }while (selected == DialogResult.Yes) ;
+    }
+
+    private void btnFor_Click(object sender, EventArgs e)
+    {
+      for (int i = 5; i > 0; i--)
+      {
+        MessageBox.Show("The great hights we have risen to just means we have further to fall. " + i, "Do");
+      }
+    }
+  }
+}
